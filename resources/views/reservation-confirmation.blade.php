23e8af51bfd6a65a4e4fb028f2818f4e68c9541c
gs-hide <hide.myhathat007@gmail.com>
2023-10-03 11:53:03 +0000

    init

---
+++ resources/views/reservation-confirmation.blade.php
@@ -0,0 +1,16 @@
+<!--<div>-->
+    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+<!--</div>-->
+<!--@extends('layouts.app')-->
+
+<!--@section('content')-->
+<!--    <div class="container mx-auto mt-8">-->
+<!--        <div class="bg-white p-6 rounded shadow">-->
+<!--            <h1 class="text-2xl font-bold mb-4">予約確認</h1>-->
+            <!-- 予約確認の内容を表示 -->
+<!--            <div>-->
+                <!-- ここに予約の詳細情報を表示 -->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--@endsection-->
